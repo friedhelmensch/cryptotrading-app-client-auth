7858b1034351c52e5a97c6621e4e2b23614cf7a5
Hannes <hannes@rang.de>
2021-05-16 20:17:14 +0200

    update npm packages

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { withAuthenticator } from 'aws-amplify-react'; // or 'aws-amplify-react-native';
-import Amplify from 'aws-amplify';
-
-Amplify.configure({
-  Auth: {
-    identityPoolId: 'eu-central-1:82ff2654-54af-40d5-92e7-c514238dabb4',
-    region: 'eu-central-1',
-    userPoolId: 'eu-central-1_ujpUzAXDs',
-    userPoolWebClientId: '2ni619b4r946j44k7r7j1l25g3',
-    mandatorySignIn: false,
-  }
-});
-
-
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import Amplify from "aws-amplify";
+import awsconfig from "./aws-exports";
+Amplify.configure(awsconfig);
 
 class App extends Component {
   render() {
@@ -22,9 +12,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Success! Now head over to the front end and log in
-          </p>
+          <p>Success! Now head over to the front end and log in</p>
           <a
             className="App-link"
             href="https://d1lo1fxbzn7aqu.cloudfront.net/login"
